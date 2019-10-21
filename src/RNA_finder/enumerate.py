a3ce708224964e3cc7b4c353e3f447617097e9c1
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2019-10-20 21:22:04 -0700

    renaming and refactoring

--- src/RNA_finder/enumerate.py
+++ src/RNA_finder/enumerate.py
@@ -3,12 +3,18 @@ import matplotlib.pyplot as plt
 
 # Enumerating RNA Structures
 
-def can_pair(A, B):
-    if A == 'A' and B == 'U' or A == 'U' and B == 'A':
+def can_pair(a, b):
+    if a == 'A' and b == 'U':
         return True
-    elif A == 'G' and B == 'C' or A == 'C' and B == 'G':
+    elif a == 'U' and b == 'A':
         return True
-    elif A == 'G' and B == 'C' or A == 'C' and B == 'G':
+    elif a == 'G' and b == 'C':
+        return True
+    elif a == 'C' and b == 'G':
+        return True
+    elif a == 'G' and b == 'U':
+        return True
+    elif a == 'U' and b == 'G':
         return True
     else:
         return False
@@ -20,7 +26,8 @@ def possible_pairs(sequence):
     for i in range(N):
         for j in range(N):
             if can_pair(sequence[i], sequence[j]):
-                B[i][j] = 1
+                B[i, j] = 1
+                B[j, i] = 1
     return B
 
 
@@ -33,11 +40,10 @@ def possible_stems(pair_matrix):
 
     minStemLength = 2
     minHairPin = 3
-    numStems = 0
-
+    maxI = N- 2*minStemLength - minHairPin+ 1
     for i in range(N):
-
-        for j in range(N-1, i, -1):
+        minJ = i + 2*minStemLength + minHairPin - 2
+        for j in range(N-1, minJ, -1):
 
             if pair_matrix[i, j] == 1:
 
@@ -47,14 +53,16 @@ def possible_stems(pair_matrix):
 
                     lenStem = 0
                     endStem = False
-
                     while not endStem:
 
                         lenStem += 1
                         new_i = i + lenStem
                         new_j = j - lenStem
 
-                        if new_i > N or new_j < 0 or abs(new_j - new_i) <= minHairPin or pair_matrix[new_i, new_j] == 0:
+                        if new_i > N-1 or \
+                         new_j < 0 or new_j - new_i <= minHairPin or \
+                         pair_matrix[new_i, new_j] == 0:
+
                             endStem = True
                             lenStem -= 1
                         else:
@@ -76,8 +84,6 @@ def mutualStems(stem1, stem2):
                 return False
     return True
 
-
-
 def compatiblilityMatrix(possibleStems):
     N_stems = len(possibleStems)
     compatibility_matrix = np.zeros((N_stems, N_stems))
@@ -90,16 +96,9 @@ def compatiblilityMatrix(possibleStems):
                 stem2 = possibleStems[j]
 
                 if mutualStems(stem1, stem2):
-                    compatibility_matrix[i,j] = 1
-
+                    compatibility_matrix[i, j] = 1
+                    compatibility_matrix[j, i] = 1
     return compatibility_matrix
-
-
-seq = 'UUCGCAGCGAAUUCUACAGUCAAUAUGGUG'
-
-mat = possible_pairs(seq)
-stems = possible_stems(mat)
-compat = compatiblilityMatrix(stems)
 
 def isCompatible(array_of_stems, next_stem):
     for i in range(len(array_of_stems)):
@@ -110,11 +109,30 @@ def isCompatible(array_of_stems, next_stem):
     return True
 
 
-def compatibileStems(matrix, stems):
+def compatibleStems(matrix, stems):
     N_stems = len(stems)
-    compatibileStems = []
-    compatibleStems.append(stems[0])
+    compat_Stems= []
     for i in range(N_stems):
         for j in range(N_stems):
-            if i != j and isCompatible(compatibleStems, ):
-                compatibleStems.append(compatibleStems, )
+            if i != j and matrix[i, j] == 1:
+                stemRegion1 = stems[i]
+                stemRegion2 = stems[j]
+                if len(compat_Stems) == 0:
+                    if mutualStems(stemRegion1, stemRegion2):
+                        compat_Stems.append(stemRegion1)
+                    else:
+                        if isCompatible(compat_Stems, stemRegion1):
+                            compat_Stems.append(stemRegion1)
+
+    return compat_Stems
+
+
+
+#seq = 'CGGUCGGAACUCGAUCGGUUGAACUCUAUC'
+seq = 'GUUAGCACAUCGAGCGGGCAAUAUGUACAU'
+mat = possible_pairs(seq)
+stems = possible_stems(mat)
+compat = compatiblilityMatrix(stems)
+poss_stems = compatibleStems(compat, stems)
+for i in range(len(stems)):
+    print(stems[i])
