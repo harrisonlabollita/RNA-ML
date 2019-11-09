e3976e8ba8ac40de92d29f8651b0a67fb7f33612
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2019-11-08 17:24:31 -0700

    fixed issue with the rates

--- src/kinetic_RNA/gillespie.py
+++ src/kinetic_RNA/gillespie.py
@@ -50,16 +50,27 @@ class Gillespie:
             out.append(x[i][1])
         return out
 
-    def calculateStemRates(self, values, kB, T):
+    def calculateStemRates(self, values, kB, T, kind):
         k_0 = 1.0
         transitionRates = []
-        for i in range(len(values)):
-            rate = k_0*np.exp(values[i]/(kB*T))
-            transitionRates.append(rate)
+        if kind:
+            # then we are calculating the rates of forming stems
+            for i in range(len(values)):
+                rate = [k_0*np.exp(values[i]/(kB*T)), 1]
+                transitionRates.append(rate)
+        else:
+
+            # then we are calculating the rates of breaking stems
+            for i in range(len(values)):
+                rate = [k_0*np.exp(values[i]/(kB*T)), 0]
+                transitionRates.append(rate)
+
         return transitionRates
 
     def calculateTotalFlux(self, rates):
-        totalFlux = sum(rates)
+        totalFlux = 0
+        for i in range(len(rates)):
+            totalFlux += rates[i][0]
         return totalFlux
 
     def isCompatible(self, stemsInStructure, j, compatibilityMatrix):
@@ -75,6 +86,11 @@ class Gillespie:
                 com.append(j)
         return com
 
+    def partialSum(self, rates):
+        partial = 0
+        for i in range(len(rates)):
+            partial += rates[i][0]
+        return partial
 
     def canAdd(self, stems, new_stem):
         s = np.ravel(stems)
@@ -89,91 +105,118 @@ class Gillespie:
     # Following Dykeman 2015 (Kfold) paper
 
         if len(self.currentStructure) == 0:
+            self.MemoryOfStems = self.STableBPs # this will be used for when we need to access a stem that we are breaking
 
             r1 = np.random.random()
             r2 = np.random.random()
-            self.ratesForm = self.calculateStemRates(self.stemEntropies, kB =  0.0019872, T = 310.15)
-            #self.ratesBreak = self.calculateStemRates(self.stemEnergies, kB = 0.0019872, T = 310.15)
+            self.rates = self.calculateStemRates(self.stemEntropies, kB =  0.0019872, T = 310.15, kind = 1)
+            self.ratesBreak = self.calculateStemRates(self.stemEnergies, kB = 0.0019872, T = 310.15, kind = 0)
+            self.totalFlux = self.calculateTotalFlux(self.rates)
 
-            self.totalFlux = self.calculateTotalFlux(self.ratesForm)
             self.time = (-1)*np.log(r2)/self.totalFlux
-            for i in range(len(self.ratesForm)):
-                trial = sum(self.ratesForm[:i])
+
+            for i in range(len(self.rates)):
+
+                trial = self.partialSum(self.rates[:i])
 
                 if  trial >= r1*self.totalFlux:
+                    # at this point we can only form a stem so we do not need to check whether this is a forming move or a breaking move
                     nextMove = self.STableBPs[i]
-
                     self.currentStructure.append(nextMove)
                     self.stemsInStructure.append(i)
+                    self.rates.append(self.ratesBreak[i]) # append the rate of breaking for the stem that we have just added. Now as part of our ensemble of
+                                                          # of possible moves, we could choose to break this stem.
+
                 # remove the chosen stem from the list
                     del self.STableBPs[i]
-                    del self.ratesForm[i]
+                    del self.rates[i]
 
                     if self.makeOutputFile:
                         self.f.write('Forming stems....\n')
                         for k in range(len(nextMove)):
                             self.f.write('Pair: %s - %s\n' %(str(nextMove[k][0]), str(nextMove[k][1])))
-                    self.totalFlux = r1*self.totalFlux - sum(self.ratesForm[:i]) # recalculate the flux
+                    self.totalFlux = r1*self.totalFlux - self.partialSum(self.rates[:i]) # recalculate the totaFlux
                     break
-
         else:
-
+            # we will always be in this part of the code after we have made our first stem
             r1 = np.random.random()
             r2 = np.random.random()
 
             self.time = self.time + (np.log(r2)/self.totalFlux)
 
-            for i in range(len(self.ratesForm)):
-                trial = sum(self.ratesForm[:i])
-                if  trial >= r1*self.totalFlux:
+            for i in range(len(self.rates)):
 
+                trial = sum(self.rates[:i])
+
+                if  trial >= r1*self.totalFlux:
                     if i >= len(self.STableBPs):
                         break
+                    # note: if rates[i][1] == 1 this is a forming rate
+                    #       if rates[i][1] == 0 this is a breaking rate
 
-                    nextMove = self.STableBPs[i]
-                    if len(self.isCompatible(self.stemsInStructure, i, self.compatibilityMatrix)) == 0:
-                        if self.canAdd(self.currentStructure, nextMove) and i not in self.stemsInStructure:
-                            self.currentStructure.append(nextMove)
-                            self.stemsInStructure.append(i)
-                        # remove the stem and the rate
-                            del self.STableBPs[i]
-                            del self.ratesForm[i]
+                    if self.rates[i][1]:
+                        nextMove = self.STableBPs[i]
+                        if len(self.isCompatible(self.stemsInStructure, i, self.compatibilityMatrix)) == 0:
+                            if self.canAdd(self.currentStructure, nextMove) and i not in self.stemsInStructure:
+                                self.currentStructure.append(nextMove)
+                                self.stemsInStructure.append(i)
+                                self.rates.append(self.ratesBreak[i]) # we will now append the opportunity to break this stem in the future
+                                # remove the stem and the rate
+                                del self.STableBPs[i] # remove this chosen stem from the possible formed stems
+                                del self.ratesForm[i]
+
                             if self.makeOutputFile:
                                 self.f.write('Forming stems...\n')
                                 for k in range(len(nextMove)):
                                     self.f.write('Pair: %s - %s\n' %(str(nextMove[k][0]), str(nextMove[k][1])))
+                            self.totalFlux = r1*self.totalFlux - self.partialSum(self.rates[:i])
 
-
-                            self.totalFlux = r1*self.totalFlux - sum(self.ratesForm[:i])
-                    else:
+                        else:
+                            break
                         # The next move is not compatible with the the current folded structure. So we will need to break the incompatible parts
                         # of the structure
 
-                        inCompatible = self.isCompatible(self.stemsInStructure, i, self.compatibilityMatrix) # finds all of the incompatible stems from the compatibility matrix
+                            #inCompatible = self.isCompatible(self.stemsInStructure, i, self.compatibilityMatrix) # finds all of the incompatible stems from the compatibility matrix
+                            #inCompList = sorted([self.STableBPs[m] for m in range(len(inCompatible))]) # sort the list in such a way so that we can remove the incompatible elements
 
-                        inCompList = sorted([self.STableBPs[m] for m in range(len(inCompatible))]) # sort the list in such a way so that we can remove the incompatible elements
-
-                        if len(inCompList) < len(self.currentStructure):
-                             # if we need to break more stems than have formed then this is not a good move at all.
+                            #if len(inCompList) < len(self.currentStructure):
+                                # if we need to break more stems than have formed then this is not a good move at all.
                              #check to make sure we are allowed to break the stems
-                            if self.makeOutputFile:
-                                self.f.write('Breaking stems...%s\n' %(str(inCompList)))
-                            for d in range(len(inCompList)):
-                                del self.currentStructure[d]
-                                del self.stemsInStructure[d]
+                            # if self.makeOutputFile:
+                            #    self.f.write('Breaking stems...%s\n' %(str(inCompList)))
+                            #for d in range(len(inCompList)):
+                            #    del self.currentStructure[d]n
+                            #    del self.stemsInStructure[d]
 
-                            if self.canAdd(self.currentStructure, nextMove) and i not in self.stemsInStructure:
+                            #if self.canAdd(self.currentStructure, nextMove) and i not in self.stemsInStructure:
 
-                                self.currentStructure.append(nextMove) # add the next move to the current structure
-                                self.stemsInStructure.append(i)
-                                del self.STableBPs[i]
-                                del self.ratesForm[i]
-                                if self.makeOutputFile:
-                                    self.f.write('Forming stems...\n')
-                                    for k in range(len(nextMove)):
-                                        self.f.write('Pair: %s - %s\n' %(str(nextMove[k][0]), str(nextMove[k][1])))
-                                self.totalFlux = r1*self.totalFlux - sum(self.ratesForm)
-                                break
+                            #    self.currentStructure.append(nextMove) # add the next move to the current structure
+                            #    self.stemsInStructure.append(i)
+                            #    del self.STableBPs[i]
+                            #    del self.ratesForm[i]
+                            #    if self.makeOutputFile:
+                            #        self.f.write('Forming stems...\n')
+                            #        for k in range(len(nextMove)):
+                            #            self.f.write('Pair: %s - %s\n' %(str(nextMove[k][0]), str(nextMove[k][1])))
+                            #    self.totalFlux = r1*self.totalFlux - sum(self.rates)
+                            #    break
+                else:
+                    # we have now choosen to break this stem
+                    # if we have choosen to break the stem then we will remove it from our current stucture and remove this move from our ensemble of moves
+                    # rates[i][1] == 0
+                    # this corresponds to choosing to break the i'th stem which is in our current structure
+                    for j in range(len(self.ratesBreak)):
+                        if rates[i][0] == self.ratesBreak[j]:
+                            breakThisStem = self.MemoryOfStem[j]
+                            # then we have found the rate that matches the j'th stem that we need to remove from our current Structure:
+                            for k in range(len(self.currentStructure)):
+                                if self.currentStructure[j] == breakThisStem:
+                                    del self.currentStructure[j]
+                                    break
+                                else:
+                                    print('Error: Move chosen to break stem %s, but can not find this stem in the current structure!' %(str(breakThisStem)))
+                            self.totalFlux = r1*self.totalFlux - self.partialSum(self.rates[:i])
+                            break
         return(self)
 
     def runGillespie(self):
@@ -224,7 +267,7 @@ class Gillespie:
 #    structure = G.runGillespie()
 #    print(structure)
 
-G = Gillespie('CGGUCGGAACUCGAUCGGUUGAACUCUAUC', [[0, 16], [1,15]], 2)
+G = Gillespie('CGGUCGGAACUCGAUCGGUUGAACUCUAUC', [], 2)
 
 #G = Gillespie('CGGUCGGAACUCGAUCGGUUGAACUCUAUC', [], 2)
 structure = G.runGillespie()
