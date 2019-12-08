9f66aa2fd66f13bbb85d63d1b7a41a6c1ce299d8
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2019-12-07 22:30:57 -0700

    updating repository

--- src/kinetic_RNA/gillespie.py
+++ src/kinetic_RNA/gillespie.py
@@ -246,39 +246,12 @@ class Gillespie:
                 answer = averages[i][0]
                 max = averages[i][1]
         return answer
+#'CGGUCGGAACUCGAUCGGUUGAACUCUAUC'
+#UGCCUGGCGGCCGUAGCGCGGUGGUCCCACCUGACCCCAUGCCGAACUCAGAAGUGAAACGCCGUAGCGCCGAUGGUAGUGUGGGGUCUCCCCAUGCGAGAGUAGGGAACUGCCAGGCAU
 
+G = Gillespie('GGGGACCCCGCGCACCCGCCAGAGCCCGUUGACCCUUGCUGCCUUCCGGCCCUGGGGGAGUUCACAGGAUGGACGCCGCGCGGGGUCC', [], maxTime = 5,toPrint = True)
+structure = G.runGillespie()
 
-def readFile(file):
-    f = open(file, 'r')
-    for i, line in enumerate(f):
-        if i == 1:
-            seq = line.rstrip()
-        if i == 2:
-            dots = line.split(' ')[1].rstrip()
-    return seq, dots
-
-def compareStructs(pred, answer):
-    count = 0
-    for i in range(len(pred)):
-        if pred[i] != answer[i]:
-            count +=1
-    return count
-
-seq_files = glob.glob('/Users/harrisonlabollita/Library/Mobile Documents/com~apple~CloudDocs/Arizona State University/Sulc group/data_set/bad_seq/*')
-
-total_misses = []
-for file in seq_files:
-    seq, dot = readFile(file)
-    G = Gillespie(seq, [], maxTime = 5, toPrint = False)
-    structure = G.runGillespie()
-    mistakes = compareStructs(structure, dot)
-    mistakes /= len(seq)
-    total_misses.append(mistakes)
-
-print('Accuracy:' (total_misses.count(0))/len(total_misses))
-print('Mean:', np.mean(total_misses))
-print('Max:', np.max(total_misses))
-print('Min:', np.max(total_misses))
 
 ################################# EXAMPLE ######################################
 #G = Gillespie('CGGUCGGAACUCGAUCGGUUGAACUCUAUC', [], maxTime = 30,toPrint = True)#
